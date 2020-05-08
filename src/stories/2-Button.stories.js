b7d98b761d142dd5c9f596787d54162aca4c5d37
laurage <laurage_@hotmail.com>
2020-05-08 01:54:55 +0100

    Updates

--- src/stories/2-Button.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from '../components/Button/Button';
-
-export default {
-  component: Button,
-  title: 'Button',
-};
-
-export const Default = () => <Button>Call to Action</Button>;
--- src/utilities/comp.js
+++
@@ -1,6 +0,0 @@
-export default function comp(styles, ...args) {
-  const stringifiedStyles = args.map(styleName => {
-    return styles[styleName]
-  })
-  return stringifiedStyles.join(' ')
-}
