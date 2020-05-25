023f94df14f2ec75a57dfec86b0fe067a8bc8524
laurage <laurage_@hotmail.com>
2020-05-25 15:43:07 +0100

    added pictures

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -2,10 +2,15 @@ import React from 'react'
 import Text from '../Text/Text'
 import { ReactComponent as PageBreak } from '../../static/images/HalfMoonLogo_vfinal.svg'
 import styles from './Page.module.css'
+import imageHome from '../../static/images/halfMoonTarot_cards_1.jpg'
+import imageReading from '../../static/images/halfMoonTarot_cards_2.jpg'
+import imageContact from '../../static/images/halfMoonTarot_cards_4.jpg'
 
 export default function ({pageType, children}) {
-  if ((pageType === "isPhilosophy") || (pageType === "isReadings")) {
-    return (<Text variant='body'>{children}</Text>)
+  if (pageType === "isPhilosophy") {
+    return (<React.Fragment><Text variant='body'>{children}</Text><img className={styles.image} src={imageHome} alt="Tarot cards" /></React.Fragment>)
+  } else if (pageType === "isReadings") {
+    return (<React.Fragment><Text variant='body'>{children}</Text><img className={styles.image} src={imageReading} alt="Tarot cards" /></React.Fragment>)
   } else if (pageType === "isSessions") {
     return (
       <React.Fragment>
@@ -21,6 +26,7 @@ export default function ({pageType, children}) {
       <React.Fragment>
         <div><Text variant='body'>{children}</Text></div>
         <div className={styles.email}><a href="mailto: hello@halfmoontarot.com"><Text variant='body'>Message Lo</Text></a></div>
+        <img className={styles.image} src={imageContact} alt="Tarot cards" />
       </React.Fragment>
     )
   }
